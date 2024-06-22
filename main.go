aa996d22d33b80f46fbb7bd90956aceff3aa591d
JinnJarBurger <adnanhossain274@gmail.com>
2024-06-22 19:15:14 +0600

    Add Commit && Log Arg Support

--- main.go
+++ main.go
@@ -2,100 +2,130 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
 	"errors"
-	"flag"
 	"fmt"
+	"io"
 	loggger "log"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
-var supportedCommands = []string{"config", "add", "log", "commit", "checkout"}
+const helpMsg = `These are SVCS commands:
+config     Get and set a username.
+add        Add a file to the index.
+log        Show commit logs.
+commit     Save changes.
+checkout   Restore a file.`
 
 func main() {
-	var config, add, log, commit, checkout string
+	loggger.SetFlags(loggger.LstdFlags | loggger.Lshortfile)
 
-	flag.StringVar(&config, "config", "", "Get and set a username.")
-	flag.StringVar(&add, "add", "", "Add a file to the index.")
-	flag.StringVar(&log, "log", "", "Show commit logs.")
-	flag.StringVar(&commit, "commit", "", "Save changes.")
-	flag.StringVar(&checkout, "checkout", "", "Restore a file.")
+	processHelpArg()
 
-	processArgs()
-	processInvalidArg()
+	err := os.MkdirAll("./vcs/commits/", os.ModePerm)
+	if err != nil {
+		loggger.Fatal(err)
+	}
 
-	err := os.MkdirAll("./vcs", os.ModePerm)
-	logError(err)
-
-	configFile, err := createOrOpenFile("config.txt")
-	logError(err)
+	configFile, err := createOrOpenFile("./vcs/", "config.txt")
+	if err != nil {
+		loggger.Fatal(err)
+	}
 	defer closeFile(configFile)
 
-	indexFile, err := createOrOpenFile("index.txt")
-	logError(err)
+	indexFile, err := createOrOpenFile("./vcs/", "index.txt")
+	if err != nil {
+		loggger.Fatal(err)
+	}
 	defer closeFile(indexFile)
 
-	processConfigFlag(config, configFile)
-	processAddFlag(add, indexFile)
-}
-
-func processInvalidArg() {
-	if os.Args[1] == "config" || os.Args[1] == "add" {
-		return
+	logFile, err := createOrOpenFile("./vcs/", "log.txt")
+	if err != nil {
+		loggger.Fatal(err)
 	}
+	defer closeFile(logFile)
 
-	if flag.Lookup(os.Args[1]) == nil {
+	switch os.Args[1] {
+	case "config":
+		processConfigArg(configFile)
+
+	case "add":
+		processAddArg(indexFile)
+
+	case "log":
+		processLogArg(logFile)
+
+	case "commit":
+		processCommitArg(configFile, indexFile, logFile)
+
+	case "checkout":
+		fmt.Println("Restore a file.")
+
+	default:
 		fmt.Printf("'%s' is not a SVCS command.", os.Args[1])
-	} else {
-		fmt.Println(flag.Lookup(os.Args[1]).Usage)
+
 	}
 }
 
-func processConfigFlag(config string, configFile *os.File) {
-	if os.Args[1] != "config" {
-		return
-	}
+func processConfigArg(configFile *os.File) {
+	config := func() string {
+		if len(os.Args) > 2 {
+			return os.Args[2]
+		}
 
-	if len(os.Args) > 2 {
-		config = os.Args[2]
-	}
+		return ""
+	}()
 
 	if config != "" {
 		err := os.Truncate(configFile.Name(), 0)
-		logError(err)
+		if err != nil {
+			loggger.Fatal(err)
+		}
 
 		_, err = fmt.Fprintln(configFile, config)
-		logError(err)
+		if err != nil {
+			loggger.Fatal(err)
+		}
 
 		fmt.Printf("The username is %s.\n", config)
-	} else {
-		scanner := bufio.NewScanner(configFile)
-		scanner.Scan()
 
-		if scanner.Text() != "" {
-			fmt.Printf("The username is %s.\n", scanner.Text())
-		} else {
-			fmt.Println("Please, tell me who you are.")
-		}
-	}
-}
-
-func processAddFlag(add string, indexFile *os.File) {
-	if os.Args[1] != "add" {
 		return
 	}
 
-	if len(os.Args) > 2 {
-		add = os.Args[2]
+	scanner := bufio.NewScanner(configFile)
+	scanner.Scan()
+
+	if scanner.Text() != "" {
+		fmt.Printf("The username is %s.\n", scanner.Text())
+	} else {
+		fmt.Println("Please, tell me who you are.")
 	}
+}
+
+func processAddArg(indexFile *os.File) {
+	add := func() string {
+		if len(os.Args) > 2 {
+			return os.Args[2]
+		}
+
+		return ""
+	}()
 
 	if add != "" {
 		if _, err := os.Stat(add); errors.Is(err, os.ErrNotExist) {
-			fmt.Printf("Can't find '%s'.", add)
+			fmt.Printf("Can't find '%s'.\n", add)
 		} else {
 			_, err := fmt.Fprintln(indexFile, add)
-			logError(err)
+			if err != nil {
+				loggger.Fatal(err)
+			}
 
-			fmt.Printf("The file '%s' is tracked.", add)
+			fmt.Printf("The file '%s' is tracked.\n", add)
 		}
 	} else if fileInfo, _ := indexFile.Stat(); fileInfo.Size() > 0 {
 		scanner := bufio.NewScanner(indexFile)
@@ -110,50 +140,322 @@ func processAddFlag(add string, indexFile *os.File) {
 	}
 }
 
-func processArgs() {
+func processLogArg(logFile *os.File) {
+	if fileInfo, _ := logFile.Stat(); fileInfo.Size() == 0 {
+		fmt.Println("No commits yet.")
+
+		return
+	}
+
+	scanner := bufio.NewScanner(logFile)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func processCommitArg(configFile, indexFile, logFile *os.File) {
+	var filesToCommit []string
+	var finalHash []byte
+	var log strings.Builder
+
+	md5Hash := md5.New()
+	md5Hash.Write([]byte(time.Now().String()))
+
+	commit := func() string {
+		if len(os.Args) > 2 {
+			return strings.ReplaceAll(os.Args[2], "\"", "")
+		}
+
+		return ""
+	}()
+
+	if commit == "" {
+		fmt.Println("Message was not passed.")
+
+		return
+	}
+
+	if fileInfo, _ := indexFile.Stat(); fileInfo.Size() == 0 {
+		fmt.Println("Nothing to commit.")
+
+		return
+	}
+
+	latestCommitDir := findLatestCommitDir()
+	bufferForFileName := bytes.Buffer{}
+	trackedFilesChanged := anyFileChanged(indexFile, latestCommitDir, &bufferForFileName)
+	scanner := bufio.NewScanner(&bufferForFileName)
+
+	for scanner.Scan() {
+		filename := scanner.Text()
+
+		dirEmpty, err := commitDirEmpty()
+		if err != nil {
+			loggger.Fatal(err)
+		}
+
+		// TODO: initially optimized space here, not needed for now but will bring this feature back later
+		if dirEmpty || trackedFilesChanged {
+			fileInIndexFile, err := openFile("./", filename)
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+			fileHash := md5.New()
+
+			_, err = io.Copy(fileHash, fileInIndexFile)
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+			finalHash = xorMd5Hashes(md5Hash.Sum(nil), fileHash.Sum(nil))
+
+			filesToCommit = append(filesToCommit, filename)
+
+			closeFile(fileInIndexFile)
+		}
+	}
+
+	if len(filesToCommit) > 0 {
+		hashDir := "./vcs/commits/" + hex.EncodeToString(finalHash) + "/"
+
+		err := os.MkdirAll(hashDir, os.ModePerm)
+		if err != nil {
+			loggger.Fatal(err)
+		}
+
+		for _, filename := range filesToCommit {
+			file, err := openFile("./", filename)
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+			destFile, err := os.Create(hashDir + filename)
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+			_, err = io.Copy(destFile, file)
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+			closeFile(destFile)
+			closeFile(file)
+		}
+
+		log.WriteString("commit ")
+		log.WriteString(hex.EncodeToString(finalHash))
+		log.WriteString("\n")
+
+		scanner := bufio.NewScanner(configFile)
+		scanner.Scan()
+
+		log.WriteString("Author: ")
+		log.WriteString(scanner.Text())
+		log.WriteString("\n")
+
+		log.WriteString(commit)
+
+		tmpLogFile, err := os.Create("./vcs/tmpLog.txt")
+		if err != nil {
+			loggger.Fatal(err)
+		}
+
+		_, err = fmt.Fprintln(tmpLogFile, log.String())
+		if err != nil {
+			loggger.Fatal(err)
+		}
+
+		scanner = bufio.NewScanner(logFile)
+
+		for scanner.Scan() {
+			_, err = fmt.Fprintln(tmpLogFile, scanner.Text())
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+		}
+
+		err = tmpLogFile.Sync()
+		if err != nil {
+			loggger.Fatal(err)
+		}
+
+		closeFile(tmpLogFile)
+
+		err = os.Rename(tmpLogFile.Name(), logFile.Name())
+		if err != nil {
+			loggger.Fatal(err)
+		}
+
+		fmt.Println("Changes are committed.")
+	} else {
+		fmt.Println("Nothing to commit.")
+	}
+
+	//err := os.Truncate(indexFile.Name(), 0)
+	//if err != nil {
+	//	loggger.Fatal(err)
+	//}
+}
+
+func anyFileChanged(indexFile *os.File, commitHash string, buffer *bytes.Buffer) bool {
+	scanner := bufio.NewScanner(indexFile)
+
+	for scanner.Scan() {
+		filename := scanner.Text()
+
+		_, err := fmt.Fprintln(buffer, filename)
+		if err != nil {
+			loggger.Fatal(err)
+		}
+
+		if fileChangedSinceLastCommit(filename, commitHash) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func fileChangedSinceLastCommit(filename, commitHash string) bool {
+	commitHashDir := "./vcs/commits/" + commitHash + "/"
+
+	file, err := openFile("./", filename)
+	if err != nil {
+		loggger.Fatal(err)
+	}
+	defer closeFile(file)
+
+	md5Hash1 := calculateMd5(file)
+
+	entries, err := os.ReadDir(commitHashDir)
+	if err != nil {
+		loggger.Fatal(err)
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() && filename == entry.Name() {
+			fileInfo, err := entry.Info()
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+			commitFile, err := openFile(commitHashDir, filepath.Base(fileInfo.Name()))
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+			md5Hash2 := calculateMd5(commitFile)
+
+			closeFile(commitFile)
+
+			return md5Hash1 != md5Hash2
+		}
+	}
+
+	return false
+}
+
+func findLatestCommitDir() string {
+	commitDir := "./vcs/commits/"
+
+	entries, err := os.ReadDir(commitDir)
+	if err != nil {
+		loggger.Fatal(err)
+	}
+
+	var latestDirName string
+	var latestCreatedTime int64
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			fileInfo, err := entry.Info()
+			if err != nil {
+				loggger.Fatal(err)
+			}
+
+			createdTime := fileInfo.ModTime().Unix()
+
+			if createdTime > latestCreatedTime {
+				latestCreatedTime = createdTime
+				latestDirName = entry.Name()
+			}
+		}
+	}
+
+	return latestDirName
+}
+
+func commitDirEmpty() (bool, error) {
+	commitDir := "./vcs/commits/"
+
+	dir, err := os.Open(commitDir)
+	if err != nil {
+		loggger.Fatal(err)
+	}
+
+	defer closeFile(dir)
+
+	_, err = dir.Readdirnames(1)
+
+	if errors.Is(err, io.EOF) {
+		return true, nil
+	} else if err != nil {
+		return false, err
+	}
+
+	return false, nil
+}
+
+func calculateMd5(file *os.File) string {
+	md5Hash := md5.New()
+	_, err := io.Copy(md5Hash, file)
+	if err != nil {
+		loggger.Fatal(err)
+	}
+
+	return hex.EncodeToString(md5Hash.Sum(nil))
+}
+
+func xorMd5Hashes(hash1, hash2 []byte) []byte {
+	if len(hash1) != len(hash2) {
+		loggger.Fatal(errors.New("invalid hashes"))
+	}
+
+	result := make([]byte, len(hash1))
+
+	for i := 0; i < len(hash1); i++ {
+		result[i] = hash1[i] ^ hash2[i]
+	}
+
+	return result
+}
+
+func processHelpArg() {
 	if len(os.Args) == 1 || (len(os.Args) > 1 && os.Args[1] == "--help") {
-		printDefault()
+		fmt.Printf(helpMsg)
 
 		os.Exit(0)
-	} else {
-		flag.Parse()
 	}
 }
 
-func printDefault() {
-	fmt.Println("These are SVCS commands:")
-
-	for _, command := range supportedCommands {
-		fmt.Printf("%-10s %s\n", command, flag.Lookup(command).Usage)
-	}
-}
-
-func isFlagPassed(name string) bool {
-	found := false
-
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
-		}
-	})
-
-	return found
-}
-
-func createOrOpenFile(filename string) (*os.File, error) {
-	if _, err := os.Stat("./vcs/" + filename); errors.Is(err, os.ErrNotExist) {
-		return os.Create("./vcs/" + filename)
+func createOrOpenFile(filepath, filename string) (*os.File, error) {
+	if _, err := os.Stat(filepath + filename); errors.Is(err, os.ErrNotExist) {
+		return os.Create(filepath + filename)
 	}
 
-	return os.OpenFile("./vcs/"+filename, os.O_APPEND|os.O_RDWR, 0644)
+	return os.OpenFile(filepath+filename, os.O_APPEND|os.O_RDWR, 0644)
+}
+
+func openFile(filepath, filename string) (*os.File, error) {
+	return os.OpenFile(filepath+filename, os.O_APPEND|os.O_RDWR, 0644)
 }
 
 func closeFile(file *os.File) {
 	err := file.Close()
-	logError(err)
-}
-
-func logError(err error) {
 	if err != nil {
 		loggger.Fatal(err)
 	}
